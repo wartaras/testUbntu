a65847c9827833a7dbed8235e4e4b936083c79e5
wartarasL <wartaras@gmail.com>
2023-03-30 14:02:22 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+def class der (self, name)
+    name = "taras"
+far = der.name
+
+Print ("far")
+
+
+# Press Ctrl+F8 to toggle the breakpoint.
+# Use a breakpoint in the code line below to debug your script.
+# press the green button in the gutter to run the script.
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
